dcee6535323c4257cbd51ae8aa38dc99b3c756ce
Omar <valis452@gmail.com>
2020-11-03 16:41:16 -0700

    adding node js and better frontend

---
+++ public/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '588a5a0d1a309ecfa8303b11be7449f4');
---
+++ public/src/index.js
@@ -0,0 +1,19 @@
+import Phaser from "phaser";
+import Game from "./scenes/game";
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: [
+        Game
+    ]
+  };
+  
+var game = new Phaser.Game(config);
